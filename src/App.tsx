7c6f9ad540b1ee15b41e0fb434a4d704a68d6579
Silvia rodriguez <silvideastora18@gmail.com>
2022-09-09 21:52:55 -0500

    Created App with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import List from './components/List'
+const INITIAL_STATE = [
+  {
+    nick: 'Dapelu',
+    subMonths:3,
+    avatar: 'https://i.pravatar.cc/150?u=dapelu',
+    description:'Dapelu hace de moderador'
+  },
+  {
+    nick: 'Silvi Rodriguez',
+    subMonths: 7,
+    avatar:'https://i.pravatar.cc/150?u=silvi'
+  }
+]
+interface Sub {
+  nick: string
+  avatar:string
+  subMonths: number
+  description?:string
+}
+interface AppState {
+  subs:Sub[]
+  newSubsNumber:number
+}
 
 function App() {
+  const [subs, setSubs]= useState<AppState["subs"]>([])
+  const [newSubsNumber, setNewSubsNumber] = useState<AppState["newSubsNumber"]>(0)
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE)
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Midu Subs</h1>
+    <List subs={subs}/>
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,25 @@
+interface Props {
+    subs: Array<{
+        nick: string
+        avatar:string
+        subMonths: number
+        description?:string
+    }>
+}
+export default function List({subs}: Props) {
+return(
+    <ul>
+      {
+        subs.map(sub => {
+          return(
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+            <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+            <p>{sub.description?.substring(0,100)}</p>
+            </li>
+          )
+        })
+      }
+    </ul>
+)
+}
